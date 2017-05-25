26a46dbfa57a2ccb0d03db3eeb178ce89b3fef30
cglascobcc <cglasco@swireprops.com>
2017-05-25 12:20:54 -0400

    Add files via upload

---
+++ iparcfeed.js
@@ -0,0 +1,17 @@
+(function () {
+    var myConnector = tableau.makeConnector();
+
+  myConnector.getSchema = function (schemaCallback) {
+    tableau.log("Hello WDC!");
+
+    myConnector.getData = function (table, doneCallback) {
+
+    };
+
+    tableau.registerConnector(myConnector);
+	$(document).ready(function () {
+    $("#submitButton").click(function () {
+        tableau.connectionName = "iParc Feed";
+        tableau.submit();
+		
+})();
